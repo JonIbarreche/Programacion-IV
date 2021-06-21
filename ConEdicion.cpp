952924eee159a7be7fbf676e1db60e9fc3f283d5
leiregago <leire.gago@opendeusto.es>
2021-06-21 11:27:27 +0200

    clase con herencia
    esta va a ser la clase con herencia de libro que serán los libros que tengan más de una edición

---
+++ ConEdicion.cpp
@@ -0,0 +1,15 @@
+/*
+ * ConEdicion.cpp
+ *
+ *  Created on: 21 jun. 2021
+ *      Author: leire
+ */
+
+#include "ConEdicion.h"
+#include <iostream>
+#include <string.h>
+
+#include "sqlite3.h"
+
+using namespace std;
+
---
+++ ConEdicion.h
@@ -0,0 +1,19 @@
+/*
+ * ConEdicion.h
+ *
+ *  Created on: 21 jun. 2021
+ *      Author: leire
+ */
+
+#ifndef CONEDICION_H_
+#define CONEDICION_H_
+
+#include "Libro.h"
+
+class ConEdicion: public Libro
+{
+
+};
+
+
+#endif /* CONEDICION_H_ */
